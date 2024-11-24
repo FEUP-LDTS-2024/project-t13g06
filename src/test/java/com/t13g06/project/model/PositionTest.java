372a1f80db3dda27bb36ecfb6f302568ff4340bc
Shad0wBD <danielsilvamarques2005@gmail.com>
2024-11-24 19:08:17 +0000

    Improved Model tests

--- src/test/java/com/t13g06/project/model/PositionTest.java
+++ src/test/java/com/t13g06/project/model/PositionTest.java
@@ -1,9 +1,10 @@
 package com.t13g06.project.model;
 
-import net.jqwik.api.ForAll;
-import net.jqwik.api.Property;
+import net.jqwik.api.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -49,8 +50,7 @@ class PositionTest {
     @Property
     void getRandomNeighbour(@ForAll int x, @ForAll int y) {
         Position position = new Position(x, y);
-        Position neighbour = position.getRandomNeighbour();
-
+        Map<Position, Integer> neighborCounts = new HashMap<>();
         Set<Position> validNeighbors = Set.of(
                 position.getUp(),
                 position.getRight(),
@@ -58,7 +58,13 @@ class PositionTest {
                 position.getLeft()
         );
 
-        assertTrue(validNeighbors.contains(neighbour));
+        for (int i = 0; i < 1000; i++) {
+            Position neighbor = position.getRandomNeighbour();
+            assertTrue(validNeighbors.contains(neighbor));
+            neighborCounts.put(neighbor, neighborCounts.getOrDefault(neighbor, 0) + 1);
+        }
+
+        assertEquals(4, neighborCounts.size());
     }
 
     @Test
@@ -70,6 +76,10 @@ class PositionTest {
         Position min = new Position(Integer.MIN_VALUE, Integer.MIN_VALUE);
         assertEquals(Integer.MIN_VALUE + 1, min.getRight().getX());
         assertEquals(Integer.MIN_VALUE, min.getRight().getY());
+
+        Position zero = new Position(0, 0);
+        assertEquals(-1, zero.getLeft().getX());
+        assertEquals(1, zero.getRight().getX());
     }
 
     @Test
@@ -78,18 +88,12 @@ class PositionTest {
         Position position2 = new Position(3, 5);
         Position position3 = new Position(4, 6);
 
-        assertEquals(position1, position1);
-
-        assertEquals(position1, position2);
-        assertEquals(position2, position1);
-
-        Position position4 = new Position(3, 5);
-        assertEquals(position1, position4);
-
         assertEquals(position1, position2);
         assertEquals(position1.hashCode(), position2.hashCode());
 
         assertNotEquals(position1, position3);
+        assertNotEquals(null, position1);
+        assertNotEquals(new Object(), position1);
     }
 
     @Test
